be220e1ae56969b27e6448d44f209a28170c745d
lzepeda <lzepeda@math.mit.edu>
2018-01-21 13:49:17 -0800

    added more scf options and eigsOptions to be used within the eigenvalue solver

--- scfOptions.jl
+++ scfOptions.jl
@@ -6,7 +6,28 @@ struct scfOptions
     betamix::Float64
     SCFtol::Float64
     scfiter::Int64
+    eigstol::Float64
+    eigsiter::Int64
+    eigmethod::AbstractString
 
-    funtion scfOptions()
-    new scfOptions('anderson',0.5,1e-7,100)
+    function scfOptions()
+        new scfOptions('anderson',0.5,1e-7,100,1e-9, 50, "eigs")
+    end
+end
+
+struct eigOptions
+    # structure to encapsulate all the different options for the scf iterations
+    # the main idea is to define a Hamiltonian, and then run scf on it; however,
+    # the Hamiltoinian shouldn't have the scf options embeded
+    eigstol::Float64
+    eigsiter::Int64
+    eigmethod::AbstractString
+
+    function eigOptions()
+        new scfOptions(1e-9, 50, "eigs")
+    end
+
+    function eigOptions(opts::scfOptions)
+        new scfOptions(opts.eigstol, opts.eigsiter, opt.eigmethod)
+    end
 end
