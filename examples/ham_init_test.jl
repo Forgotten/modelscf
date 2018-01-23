dd2470054a43406e5bd8d532817ee9441e83f861
lzepeda <lzepeda@math.mit.edu>
2018-01-23 13:33:53 -0800

    changed the organization of the files added a small notebook

--- examples/ham_init_test.jl
+++ examples/ham_init_test.jl
@@ -1,19 +1,30 @@
 #test construction of the Hamiltonian
 
-include("Atoms.jl")
-include("scfOptions.jl")
-include("Ham.jl")
-include("hartree_pot_bc.jl")
-include("pseudocharge.jl")
-include("getocc.jl")
-include("anderson_mix.jl")
+include("../src/Atoms.jl")
+include("../src/scfOptions.jl")
+include("../src/Ham.jl")
+include("../src/hartree_pot_bc.jl")
+include("../src/pseudocharge.jl")
+include("../src/getocc.jl")
+include("../src/anderson_mix.jl")
 
 dx = 1.0;
 Nunit = 32;
 Lat = 10;
+# using the default values in Lin's code
+YukawaK = 0.0100
+n_extra = 10; # QUESTION: I don't know where this comes from
+epsil0 = 10.0;
+T_elec = 100;
+
+kb = 3.1668e-6;
+au2K = 315774.67;
+Tbeta = au2K / T_elec;
+
 
 Ndist  = 1;   # Temporary variable
 Natoms = round(Integer, Nunit / Ndist);
+
 # Temp var, will be redefined by the size of R later.
 R = zeros(Natoms, 1); # this is defined as an 2D array
 for j = 1:Natoms
@@ -27,31 +38,10 @@ Eqdist = ones(Natoms,1)*10.0;
 mass   = ones(Natoms,1)*42000.0;
 nocc   = ones(Natoms,1)*2;          # number of electrons per atom
 Z      = nocc;
+# creating an atom structure
 atoms = Atoms(Natoms, R, sigma,  omega,  Eqdist, mass, Z, nocc);
 
-# using the default values in Lin's code
-YukawaK = 0.0100
-n_extra = 10; # QUESTION: I don't know where this comes from
-epsil0 = 10.0;
-
-Ls = Nunit * Lat;
-Ns = round(Integer, Ls / dx);
-
-T_elec = 100;
-
-kb = 3.1668e-6;
-au2K = 315774.67;
-Tbeta = au2K / T_elec;
-
-
-#
-dx = Ls / Ns;
-# defining the grid
-# allocating gripos as an 2D Array
-gridpos = zeros(Ns,1)
-gridpos[:,1] = collect(0:Ns-1).'.*dx; #'
-
-
+# allocating a Hamiltonian
 ham = Ham(Lat, Nunit, n_extra, dx, atoms,YukawaK, epsil0, Tbeta)
 
 # total number of occupied orbitals
@@ -60,8 +50,11 @@ Nocc = round(Integer, sum(atoms.nocc) / ham.nspin);
 # initialize the potentials within the Hemiltonian
 # setting H[\rho_0]
 init_pot!(ham, Nocc)
+
+# initializing the options
 scfOpts = scfOptions();
 
+# running the scf iteration
 VtoterrHist = scf!(ham, scfOpts)
 
 
