7dc50ae7783685b4d2b973d6a65d950ae5fb6530
lzepeda <lzepeda@math.mit.edu>
2018-01-23 12:27:22 -0800

    playing with some optimizations to the computation of the potential

--- hartree_pot_bc.jl
+++ hartree_pot_bc.jl
@@ -20,3 +20,51 @@ function hartree_pot_bc(rho::Array{Float64,2}, Ls::Float64, YukawaK::Float64, ep
     V  = real(ifft(Vtemp));
  return V / epsil0;
 end
+
+############################################################################
+##  Trying different optimizations
+
+function hartree_pot_bc_opt(rho::Array{Float64,2}, Ls::Float64, YukawaK::Float64, epsil0::Float64)
+# Calculate the Hartree potential and the energy in periodic boundary potential
+# this function is called from the one in the Ham. This is anotated to give the jit
+# compiler more information
+    rhoFourier = rfft(rho)
+    inv_yukawa_fourier_mult!(rhoFourier,Ls,YukawaK)
+    V  = irfft(rhoFourier, size(rho,1));
+    return V / epsil0;
+end
+
+
+@inline function inv_yukawa_fourier_mult!(R::Array{Complex128,2}, Ls::Float64,
+                                          YukawaK::Float64 )
+    c = (2 * pi / Ls)^2
+    c1 = 4*pi
+    R[1] = 0
+    @inbounds @simd for ii = 2:size(R)[1]
+        R[ii] = c1*R[ii]/((ii-1)^2*c + YukawaK^2);
+    end
+end
+
+
+# using 1D vector in general should produce faster resutls
+
+function hartree_pot_bc_opt_vec(rho::Vector{Float64}, Ls::Float64, YukawaK::Float64, epsil0::Float64)
+# Calculate the Hartree potential and the energy in periodic boundary potential
+# this function is called from the one in the Ham. This is anotated to give the jit
+# compiler more information
+    rhoFourier = rfft(rho)
+    inv_yukawa_fourier_mult_vec!(rhoFourier,Ls,YukawaK)
+    V  = irfft(rhoFourier, size(rho,1))/ epsil0;
+    return V;
+end
+
+
+@inline function inv_yukawa_fourier_mult_vec!(R::Vector{Complex128}, Ls::Float64,
+                                          YukawaK::Float64 )
+    c = (2 * pi / Ls)^2
+    c1 = 4*pi
+    R[1] = 0
+    @inbounds @simd for ii = 2:size(R)[1]
+        R[ii] = c1*R[ii]/((ii-1)^2*c + YukawaK^2);
+    end
+end
