ef44cf6a35c62d01b785bc8a01141651cecb0cb7
Leo <lzepeda@lbl.gov>
2019-04-09 13:03:27 -0700

    small modification

--- examples/lobpcg_test.py
+++ examples/lobpcg_test.py
@@ -15,13 +15,13 @@ A = 0.5*(A + A')
 X0 = qr(rand(Ns, k + 6)).Q[:, 1:k+6]
 
 #computing the lowest K eigenvalues
-(eL, XL, it) = lobpcg_sep(A,X0, x-> x, k, verbose=true )
+(eL, XL, it) = lobpcg_sep(A,X0, x-> x, k )
 
 # printing the error
 println("error on the computation the eigenvalues " * string(norm(eL - e[1:k])))
 
 # now we use a preconditioner (the exact inverse)
 Ainv = inv(A)
-(eL1, XL1, it1) = lobpcg_sep(A,X0, x-> Ainv*x, k, verbose=true )
+(eL1, XL1, it1) = lobpcg_sep(A,X0, x-> Ainv*x, k)
 
 println("error on the computation the eigenvalues " * string(norm(eL1 - e[1:k])))
